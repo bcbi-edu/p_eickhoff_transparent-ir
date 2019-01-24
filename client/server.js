979ccb5931f95e6056b1a3339781b24ea10a7350
jramos70 <jerome_ramos@brown.edu>
2019-01-24 01:03:34 -0500

    initial push

--- client/server.js
+++ client/server.js
@@ -50,9 +50,16 @@ app.get('/id', function(req, res) {
   curr++;
 });
 
-app.get('/simulation', function(req, res) {
-  res.sendFile('./build/index.html');
-});
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
 
 app.get('/legal', function(req, res) {
     res.sendFile(__dirname + '/legal.html');
