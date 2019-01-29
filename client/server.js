1b5f1bba688c8b1afcac4c2f440b2e5bd7c13701
jramos70 <jerome_ramos@brown.edu>
2019-01-28 22:56:38 -0500

    fix sim

--- client/server.js
+++ client/server.js
@@ -57,10 +57,10 @@ app.get('/id', function(req, res) {
 // 	res.sendFile(__dirname + '/build/index.html');
 // });
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'test_build')));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'test_build', 'index.html'));
 });
 
 app.get('/legal', function(req, res) {
