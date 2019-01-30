5b7dc43d510354a6920a88d1767bee4ad2d0ffc2
jramos70 <jerome_ramos@brown.edu>
2019-01-29 23:22:34 -0500

    changed api address

--- client/build/precache-manifest.b696ada28b22275fa51108696509aece.js
+++ client/build/precache-manifest.b696ada28b22275fa51108696509aece.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "07e0d22960aacd083cb5",
-    "url": "/static/js/main.07e0d229.chunk.js"
+    "revision": "fedf075f49ed2cdc365c",
+    "url": "/static/js/main.fedf075f.chunk.js"
   },
   {
     "revision": "6d5e12aabcf534f969b2",
     "url": "/static/js/1.6d5e12aa.chunk.js"
   },
   {
-    "revision": "07e0d22960aacd083cb5",
+    "revision": "fedf075f49ed2cdc365c",
     "url": "/static/css/main.a47b1ea3.chunk.css"
   },
   {
-    "revision": "bdeab8d3265acfaf0e838da7578f103e",
+    "revision": "0a14fec073b580c49118e71fb2c0821a",
     "url": "/index.html"
   }
 ];
